cd3aaa9f2fbe8263e2e75973494e70b1b850c35d
才楚 <caichu.fh@antgroup.com>
2022-02-10 11:51:39 +0800

    init

---
+++ src/main/java/com/alipay/deve/spademo/SpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alipay.deve.spademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alipay/deve/spademo/SpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipay.deve.spademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
